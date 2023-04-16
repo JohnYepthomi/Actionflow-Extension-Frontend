7877df2bd961ea66c526b36bd055924acd3ef918
JohnYepthomi <johnyepthomi@gmail.com>
2023-04-17 00:54:54 +0530

    changes in utility dir

--- src/utils/messageTab.js
+++ src/utils/messageTab.js
@@ -2,6 +2,5 @@ import chromeStorageGet from "./chromeStorage";
 
 export default async function messageTab(message) {
   const tabData = await chromeStorageGet("lastActiveTabData");
-  console.log("FRONTEND lastActiveTab: ", tabData);
   await chrome.tabs.sendMessage(tabData.tabId, { message });
 }
